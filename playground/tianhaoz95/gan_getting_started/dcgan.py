8675dfbe0b2a035e3aaaec15c457e17b0bd020d7
Tianhao Zhou <tianhaoz@umich.edu>
2020-09-28 09:16:26 -0700

    fix: use recommended keras import

--- playground/tianhaoz95/gan_getting_started/dcgan.py
+++ playground/tianhaoz95/gan_getting_started/dcgan.py
@@ -3,7 +3,7 @@ import os
 import matplotlib.pyplot as plt
 from utils import check_gpu, create_directory_if_not_exist, restore_previous_checkpoint
 from loguru import logger
-from tensorflow.keras import layers
+from tensorflow import keras
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
@@ -11,7 +11,7 @@ os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 proj_id = 'mnist'
 buffer_size = 60000
 batch_size = 256
-epochs = 160
+epochs = 3
 noise_dim = 100
 mean_val = 255.0 / 2.0
 interval = 5
@@ -25,9 +25,9 @@ ckpt_output_dir = os.path.join(output_dir, 'checkpoints')
 def prepare_dataset(dataset_id):
     train_imgs = None
     if dataset_id == "fashion_mnist":
-        (train_imgs, _), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
+        (train_imgs, _), (_, _) = keras.datasets.fashion_mnist.load_data()
     if dataset_id == "mnist":
-        (train_imgs, _), (_, _) = tf.keras.datasets.mnist.load_data()
+        (train_imgs, _), (_, _) = keras.datasets.mnist.load_data()
     train_imgs = train_imgs.reshape(
         train_imgs.shape[0], 28, 28, 1).astype('float32')
     train_imgs = (train_imgs - mean_val) / mean_val
@@ -37,39 +37,39 @@ def prepare_dataset(dataset_id):
 
 
 def build_generator_model():
-    model = tf.keras.Sequential()
+    model = keras.Sequential()
     # From a vector of 7*7*256 to (7,7,256)
-    model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    model.add(layers.Reshape((7, 7, 256)))
+    model.add(keras.layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Reshape((7, 7, 256)))
     # From (7, 7, 256) to (7, 7, 128)
-    model.add(layers.Conv2DTranspose(
+    model.add(keras.layers.Conv2DTranspose(
         128, (5, 5), strides=1, padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
     # From (7, 7, 128) to (14, 14, 64)
-    model.add(layers.Conv2DTranspose(
+    model.add(keras.layers.Conv2DTranspose(
         64, (5, 5), strides=2, padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.LeakyReLU())
     # From (14, 14, 64) to (28, 28, 1)
-    model.add(layers.Conv2DTranspose(
+    model.add(keras.layers.Conv2DTranspose(
         1, (5, 5), strides=2, padding='same', use_bias=False))
     return model
 
 
 def build_discriminator_model():
-    model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=2,
+    model = keras.Sequential()
+    model.add(keras.layers.Conv2D(64, (5, 5), strides=2,
                             padding='same', input_shape=(28, 28, 1)))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-    model.add(layers.Conv2D(128, (5, 5), strides=2, padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(1))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Conv2D(128, (5, 5), strides=2, padding='same'))
+    model.add(keras.layers.LeakyReLU())
+    model.add(keras.layers.Dropout(0.3))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(1))
     return model
 
 
@@ -120,8 +120,8 @@ def main():
     check_gpu(logger)
     gen = build_generator_model()
     dis = build_discriminator_model()
-    gen_opt = tf.keras.optimizers.Adam(1e-4)
-    dis_opt = tf.keras.optimizers.Adam(1e-4)
+    gen_opt = keras.optimizers.Adam(1e-4)
+    dis_opt = keras.optimizers.Adam(1e-4)
     create_directory_if_not_exist(ckpt_output_dir)
     checkpoint = tf.train.Checkpoint(generator_optimizer=gen_opt,
                                      discriminator_optimizer=dis_opt,
