684e49154088e6cebcde10b3aefd806331dc2a5b
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-09 20:03:39 -0700

    chore: add v2 models

--- playground/tianhaoz95/gan_getting_started/cond_mnist.py
+++ playground/tianhaoz95/gan_getting_started/cond_mnist.py
@@ -9,9 +9,9 @@ mean_val = 255.0 / 2.0
 def load_cond_mnist_dataset(project_id, buf_size, batch_size):
     train_imgs = None
     train_labels = None
-    if project_id == 'cond_fashion_mnist':
+    if 'cond_fashion_mnist' in project_id:
         (train_imgs, train_labels), (_, _) = keras.datasets.fashion_mnist.load_data()
-    elif project_id == 'cond_mnist':
+    elif 'cond_mnist' in project_id:
         (train_imgs, train_labels), (_, _) = keras.datasets.mnist.load_data()
     else:
         raise NotImplementedError(
@@ -38,6 +38,80 @@ class CondMnistInputGenerator():
         label = tf.one_hot([i % 10 for i in range(sample_size)], 10, dtype=tf.float32)
         return [feat, label]
 
+
+class GeneratorModel_v2(keras.Model):
+    def __init__(self):
+        super(GeneratorModel_v1, self).__init__()
+        # Expand 7*7*128 features into a (7,7,128) tensor
+        self.dense_1 = keras.layers.Dense(7*7*255)
+        self.bn_1 = keras.layers.BatchNormalization()
+        self.relu_1 = keras.layers.LeakyReLU()
+        self.reshape_1 = keras.layers.Reshape((7, 7, 256))
+        # From (7,7,256) to (7,7,128)
+        self.convt_1 = keras.layers.Conv2DTranspose(
+            128, (5, 5), strides=1, padding='same', use_bias=False)
+        self.convt_bn_1 = keras.layers.BatchNormalization()
+        self.convt_relu_1 = keras.layers.LeakyReLU()
+        # From (7,7,128) to (14,14,64)
+        self.convt_2 = keras.layers.Conv2DTranspose(
+            64, (5, 5), strides=2, padding='same', use_bias=False)
+        self.convt_bn_2 = keras.layers.BatchNormalization()
+        self.convt_relu_2 = keras.layers.LeakyReLU()
+        # From (14,14,64) to (28,28,1)
+        self.convt_out = keras.layers.Conv2DTranspose(
+            1, (5, 5), strides=2, padding='same', use_bias=False)
+
+    def call(self, inputs):
+        feat_x = inputs[0]
+        label = inputs[1]
+        x = tf.concat([feat_x, label])
+        # Expand features to image channels
+        x = self.dense_1(x)
+        x = self.bn_1(x)
+        x = self.relu_1(x)
+        x = self.reshape_1(x)
+        # From (7,7,256) to (7,7,128)
+        x = self.convt_1(x)
+        x = self.convt_bn_1(x)
+        x = self.convt_relu_1(x)
+        # From (7,7,128) to (14,14,64)
+        x = self.convt_2(x)
+        x = self.convt_bn_2(x)
+        x = self.convt_relu_2(x)
+        # From (14,14,64) to (28,28,1)
+        x = self.convt_out(x)
+        return [x, label]
+
+
+class DiscriminatorModel_v2(keras.Model):
+    def __init__(self):
+        super(DiscriminatorModel_v1, self).__init__()
+        self.conv_1 = keras.layers.Conv2D(
+            64, (5, 5), strides=2, padding='same', input_shape=(28, 28, 1))
+        self.relu_1 = keras.layers.LeakyReLU()
+        self.drop_1 = keras.layers.Dropout(0.3)
+        self.conv_2 = keras.layers.Conv2D(
+            128, (5, 5), strides=2, padding='same')
+        self.relu_2 = keras.layers.LeakyReLU()
+        self.drop_2 = keras.layers.Dropout(0.3)
+        self.flatten = keras.layers.Flatten()
+        self.dense_1 = keras.layers.Dense(128)
+        self.dense_2 = keras.layers.Dense(64)
+        self.out = keras.layers.Dense(1)
+
+    def call(self, inputs):
+        images_x = inputs[0]
+        labels = inputs[1]
+        x = self.conv_1(images_x)
+        x = self.relu_1(x)
+        x = self.drop_1(x)
+        x = self.conv_2(x)
+        x = self.relu_2(x)
+        x = self.drop_2(x)
+        x = self.flatten(x)
+        x = tf.concat([x, labels])
+        x = self.out(x)
+        return x
 
 class GeneratorModel_v1(keras.Model):
     def __init__(self):
@@ -123,16 +197,32 @@ class DiscriminatorModel_v1(keras.Model):
         return x
 
 
+def generator_factory(project_id):
+    if 'v1' in project_id:
+        return GeneratorModel_v1()
+    elif 'v2' in project_id:
+        return GeneratorModel_v2()
+    else:
+        raise NotImplemented('Project {0} not found.'.format(project_id))
+
+def discriminator_factory(project_id):
+    if 'v1' in project_id:
+        return DiscriminatorModel_v1()
+    elif 'v2' in project_id:
+        return DiscriminatorModel_v2()
+    else:
+        raise NotImplemented('Project {0} not found.'.format(project_id))
+
 def train_cond_mnist(project_id):
     check_gpu(logger)
-    buf_size = 60000
-    batch_size = 256
-    feat_dim = 100
-    project_metadata = get_metadata(project_id)
     train(
-        dataset=load_cond_mnist_dataset(project_id, buf_size, batch_size),
-        gen=GeneratorModel_v1(),
-        dis=DiscriminatorModel_v1(),
+        dataset=load_cond_mnist_dataset(
+            project_id=project_id, 
+            buf_size=60000, 
+            batch_size=256,
+        ),
+        gen=generator_factory(project_id),
+        dis=discriminator_factory(project_id),
         gen_opt=keras.optimizers.Adam(1e-4),
         dis_opt=keras.optimizers.Adam(1e-4),
         logger=logger,
@@ -144,6 +234,6 @@ def train_cond_mnist(project_id):
         batch_size=32,
         mean_val=mean_val,
         visualize=visualize_cond_mnist_sample,
-        project_metadata=project_metadata,
-        gen_input_generator=CondMnistInputGenerator(feat_dim),
+        project_metadata=get_metadata(project_id=project_id),
+        gen_input_generator=CondMnistInputGenerator(feat_dim=100),
     )
